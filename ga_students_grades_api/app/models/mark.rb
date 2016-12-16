ca32db6006e783208c49260f2d8e93144efb9fa7
Sdraje <alfmarmil@gmail.com>
2016-12-16 14:53:26 +0000

    fixed regex validation for marks

--- ga_students_grades_api/app/models/mark.rb
+++ ga_students_grades_api/app/models/mark.rb
@@ -1,14 +1,10 @@
 class Mark < ApplicationRecord
   belongs_to :user
   belongs_to :grade
-  validates :comment, presence: true, length: { minimum: 0, maximum: 142 }, allow_blank: false
+  validates :comment, presence: true, length: { minimum: 0, maximum: 140 }, allow_blank: false
 
-  validates :classwork_achievement, presence: true, format: /[ABCDEI]/, length: { minimum: 1, maximum: 1 }
-  # validates :homework_achievement, presence: true, format: /^[A-E\d\s][I]*$/
-  # validates :classwork_effort, presence: true, format: /[1-5]/
-  # validates :homework_effort, presence: true, format: /[1-5]/
+  validates :classwork_achievement, presence: true, format: /[[A-E][I]]/, length: { minimum: 1, maximum: 1 }
+  validates :homework_achievement, presence: true, format: /[[A-E][I]]/, length: { minimum: 1, maximum: 1 }
+  validates :classwork_effort, presence: true, format: /[1-5]/
+  validates :homework_effort, presence: true, format: /[1-5]/
 end
-
-# validates :classwork_achievement, presence: true, inclusion: { in: ["A", "B", "C", "D", "E", "I"], message: "%{value} is not a valid mark" }
-
-# validates :classwork_effort, presence: true, inclusion: { in: [1, 2, 3, 4, 5], message: "%{value} is not a valid mark" }
