6bcb51e5ea9fb04030c2fd679d4820f3bf0cdad3
delamorinierejh <delamorinierejh@gmail.com>
2016-12-20 15:19:02 +0000

    first full draft of api

--- ga_students_grades_api/app/serializers/cohort_serializer.rb
+++ ga_students_grades_api/app/serializers/cohort_serializer.rb
@@ -1,10 +1,10 @@
 class CohortSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_date, :end_date, :students, :instructors
+  attributes :id, :name, :start_date, :end_date, :instructors, :students
 
+  has_many :grades, through: :students
 
-  # attribute :grades, if: :is_student?
-  #
   # def is_student?
   #   true if object.student.role === 'student'
   # end
+
 end
