04daae11df26a0a97af8e0003dc540acb7a1d536
alfredomariamilano <alfmarmil@gmail.com>
2017-01-10 14:56:20 +0000

    Merging master

--- ga-students-grades-client/src/js/controllers/users/show.js
+++ ga-students-grades-client/src/js/controllers/users/show.js
@@ -1,13 +1,26 @@
 angular
-  .module('gaGradesApp')
-  .controller('UsersShowCtrl', UsersShowCtrl);
+.module('gaGradesApp')
+.controller('UsersShowCtrl', UsersShowCtrl);
 
-UsersShowCtrl.$inject = ['User', '$stateParams'];
-function UsersShowCtrl(User, $stateParams){
+UsersShowCtrl.$inject = ['User', '$stateParams', 'Grade', '$scope'];
+function UsersShowCtrl(User, $stateParams, Grade, $scope){
   const vm = this;
   User.get($stateParams, data => {
-    console.log(data);
+
     vm.user = data;
+    for (var i = vm.user.grades.length-1; i >= 0; i--) {
+      vm.user.grades[i].ticked = false;
+      Grade.get({id: vm.user.grades[i].id}, data => {
+        console.log(data);
+        var holdHere = i
+        const grade = data;
+        for (var j = 0; j < grade.marks.length; j++) {
+          if ($scope.$parent.user.id === grade.marks[j].instructor.id){
+            // vm.user.grades[holdHere].ticked = true;
+          }
+        }
+      });
+    }
   });
   // $http
   // .get('http://localhost:3000/users')
